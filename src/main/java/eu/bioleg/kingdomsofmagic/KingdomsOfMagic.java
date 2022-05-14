b1aae3a4f9f345bc6574dc0f05f4b8d4d27d8989
Bioleg <83340511+BrunosGamesUnity@users.noreply.github.com>
2022-05-14 14:29:03 +0200

    Initial commit

---
+++ src/main/java/eu/bioleg/kingdomsofmagic/KingdomsOfMagic.java
@@ -0,0 +1,35 @@
+package eu.bioleg.kingdomsofmagic;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(KingdomsOfMagic.MOD_ID)
+public class KingdomsOfMagic
+{
+    public static final String MOD_ID = "kingdomsofmagic";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public KingdomsOfMagic()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
